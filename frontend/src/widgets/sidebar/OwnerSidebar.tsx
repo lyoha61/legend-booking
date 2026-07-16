64021312e7f1b4c6cdbb7c248017446dc01242e0
lyoha61 <nightmessenger703@gmail.com>
2026-07-16 18:41:59 +0300

    feat(owner): implement navigate to search page with logo

--- frontend/src/widgets/sidebar/OwnerSidebar.tsx
+++ frontend/src/widgets/sidebar/OwnerSidebar.tsx
@@ -19,7 +19,10 @@ export const OwnerSidebar = () => {
 
 	return (
 		<div className="h-full px-3 py-4 bg-white rounded-xl shadow-lg">
-			<div className="flex items-center gap-3 mb-10">
+			<div
+				className="flex items-center gap-3 mb-10 cursor-pointer"
+				onClick={() => navigate("/search")}
+			>
 				<Building2 className="size-6 text-teal-700" />
 				<h1 className="tracking-tight bg-linear-to-r from-[#0F766E] to-[#14B8A6] bg-clip-text text-transparent text-[18px]">
 					Legend Booking
